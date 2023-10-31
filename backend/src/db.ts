c9486cfd63fd398645b3414f601c596089e36166
Laurent Vuillaume <figura.graphik@gmail.com>
2023-10-31 12:18:32 +0100

    fix main

---
+++ backend/src/db.ts
@@ -0,0 +1,10 @@
+import { DataSource } from "typeorm";
+
+export default new DataSource({
+  type: "sqlite",
+  database: "the_good_corner.sqlite",
+  entities: ["src/entities/*.ts"],
+  synchronize: true,
+  // logging: ["query","error"],
+  logging: true,
+});
--- backend/src/lib/utilities.ts
+++ backend/src/lib/utilities.ts
@@ -1,43 +1,45 @@
 import AE from "aggregate-error";
 import { ValidationError } from "class-validator";
 import { TypeORMError } from "typeorm";
-import { IError } from "../types/error";
+
+interface IError extends Error {
+  field: string | null;
+  message: string;
+}
 
 //formatage des erreurs venant de class-validator
 export function aggregateErrors(
-    errors: ValidationError[]
-    // ): AggregateError<Error> {
-  ): any {
-    const errorsFormated = errors.map((error) => {
-      console.log("ERROR DE VALIDATION : ", error);
-      if (error.constraints) {
-        // const key = Object.keys(error.constraints); // "min"
-        const key = Object.keys(error?.constraints || {})[0]; // "min"
-        console.log("KEY =============>", key);
-        console.log("PROPERTY", error.property);
-        return {
-          field: error.property,
-          message: error.constraints[key], //error.contraints["min"]
-        };
-      } else {
-        return {};
-      }
-    });
-    console.log("ERROR FORMATED =====>", errorsFormated)
-    return errorsFormated;
-}
-  
-export function formatedErrors(err: AggregateError | TypeORMError) {
-    console.log("ERROR ====>", err);
-    console.log("ERROR NAME ===>", err.name);
-    const e: any = {
-      errors: [],
-    };
-  
-    if (err.name === "AggregateError") {
-      const aggregateError = err as AE<IError>;
-      e.errors = aggregateError.errors;
+  errors: ValidationError[]
+  // ): AggregateError<Error> {
+): any {
+  const errorsFormated = errors.map((error) => {
+    console.log("ERROR DE VALIDATION : ", error);
+    if (error.constraints) {
+      // const key = Object.keys(error.constraints); // "min"
+      const key = Object.keys(error?.constraints || {})[0]; // "min"
+      console.log("KEY =============>", key);
+      console.log("PROPERTY", error.property);
+      return {
+        field: error.property,
+        message: error.constraints[key], //error.contraints["min"]
+      };
+    } else {
+      return {};
     }
-  
-    return e;
+  });
+  return errorsFormated;
+}
+
+export function formatedErrors(err: AggregateError | TypeORMError) {
+  const e: any = {
+    errors: [],
+  };
+
+  if (err.name === "AggregateError") {
+    const aggregateError = err as any;
+    // const aggregateError = err as AE<IError>;
+    e.errors = aggregateError.errors;
+  }
+
+  return e;
 }
